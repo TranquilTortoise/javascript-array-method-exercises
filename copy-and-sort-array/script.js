bb092eec706b4793ade7f3bdb34513e8654bff02
Tranquil Tortoise <SlinkTheTranquilTortoise@gmail.com>
2025-05-26 01:02:07 -1000

    fixed filesystem

---
+++ copy-and-sort-array/script.js
@@ -0,0 +1,12 @@
+// take an array and create a new array that is sorted without changing the old one
+
+function copyAndSortArray(arr) {
+
+    let sortedArr = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        sortedArr[i] = arr[i];
+    }
+
+    return sortedArr.sort;
+}

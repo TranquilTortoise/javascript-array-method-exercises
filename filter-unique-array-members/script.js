18005de7fcf4eb2ae8a4a69759935c73c240775f
Tranquil Tortoise <SlinkTheTranquilTortoise@gmail.com>
2025-05-26 20:49:41 -1000

    complete filter unique array members function

--- filter-unique-array-members/script.js
+++ filter-unique-array-members/script.js
@@ -1,6 +1,15 @@
-// take an array, and return a new array with only one copy of each element in original
+// take an array, and return a new array with only one copy of each element from the original
 
 function unique(arr) {
 
-    
+    let uniqueArr = [];
+
+    for (let str of arr) {
+
+        if (!result.includes(str)) {
+
+            result.push(str);
+        }
+    }
+    return uniqueArr;
 }
